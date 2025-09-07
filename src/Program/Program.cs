1da15cba474c4b3ea79040e57ae4f291947bb7b8
Gaston <gardilgaston@gmail.com>
2025-09-07 20:36:26 -0300

    Leerr

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,31 +6,64 @@ namespace Ucu.Poo.GameOfLife
     {
         static void Main(string[] args)
         {
-            LeerArchivo archivo = new LeerArchivo();
-            bool[,] tableroInicial = archivo.Archivo("board.txt");
+           
         }
     }
 }
-
-// Cumple SRP porque tiene la unica responsabilidad de leer y de parsear.
-// Cumple expert porque es la parte del codigo con la que se puede parsear 1/0 en una matriz bool[,] sin necesidad de otras clases
-public class LeerArchivo // Creo una clase para el snippet
-{
-    public  bool[,] Archivo(string url) // Declara una funcion de la que va a retornar un bool[,]
+/*
+ * Clase dedicada a obtener el tablero de donde sea que tenga que obtenerlo, en este caso un archivo.
+ * Cumple con SRP porque su unica funcion es obtener el contenido necesario paar crear un archivo de donde deba,
+ * si en un futuro cambia la forma de obtener el contenido para crear el archivo, por ejemplo se obtienen archivos de un gestor externo,
+ * se cambia esta clase.
+ * Cumple con Expert porque tiene la informacion necesaria para obtener el contenido del archivo, que en este caso es la url de donde esta.
+ */
+    public class ObtenerTablero
     {
-        string content = File.ReadAllText(url); // Lee todo el archivo
-        string[] contentLines = content.Split('\n');
-        bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
-        for (int y=0; y<contentLines.Length;y++)
+        public string UrlDelArchivo { get;}
+        public string Content { get; private set; }
+
+        public ObtenerTablero(string unaDireccion)
         {
-            for (int x=0; x<contentLines[y].Length; x++)
+            UrlDelArchivo = unaDireccion;
+        }
+
+        public string ObtenerContenido()
+        {
+            Content = File.ReadAllText(UrlDelArchivo);
+            
+            return Content;
+        }
+        
+    }
+    
+/*
+ * Se crea la clase tablero en donde se porcesa el contenido del archivo, guardandolo en forma de tablero.
+ * Cumple con expert ya que tiene la info necesaria para procesar el archivo, el cual se le brinda al construir la tabla.
+ * Cumple con SRP ya que esta clase solo se encarga de crear el tablero, si en un futuro cambia ese metodo se cambia esta clase.
+ */
+    public class Tablero
+    {
+        public bool[,] Board { get; private set; }
+
+        public Tablero(string content)
+        {
+            CrearTablero(content);
+        }
+        
+        private void CrearTablero(string content)
+        {
+            string[] contentLines = content.Split('\n');
+            Board = new bool[contentLines.Length, contentLines[0].Length];
+            for (int y = 0; y < contentLines.Length; y++)
             {
-                if(contentLines[y][x]=='1')
+                for (int x = 0; x < contentLines[y].Length; x++)
                 {
-                    board[x,y]=true;
+                    if (contentLines[y][x] == '1')
+                    {
+                        Board[x, y] = true;
+                    }
                 }
             }
-        } return board; // retorna el tablero
-    }   
-}
-
+            
+        }
+    }
