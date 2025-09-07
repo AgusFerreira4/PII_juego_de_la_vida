09205f099f53f8a2a12856b284a52a6192b18f57
Gaston <gardilgaston@gmail.com>
2025-09-07 17:58:36 -0300

    Implementacion de la clase LeerArchivo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 namespace Ucu.Poo.GameOfLife
 {
     class Program
@@ -10,3 +10,25 @@ namespace Ucu.Poo.GameOfLife
         }
     }
 }
+public class LeerArchivo
+{
+    public  bool[,] Archivo(string url)
+    {
+        string content = File.ReadAllText(url);
+        string[] contentLines = content.Split('\n');
+        bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
+        for (int y=0; y<contentLines.Length;y++)
+        {
+            for (int x=0; x<contentLines[y].Length; x++)
+            {
+                if(contentLines[y][x]=='1')
+                {
+                    board[x,y]=true;
+                }
+            }
+        } return board;
+    }   
+}
+
+LeerArchivo archivo = new LeerArchivo();
+bool[,] tableroInicial = archivo.Archivo("board.txt");
