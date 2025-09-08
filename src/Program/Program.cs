63ed98b2fb5d9cf8c72ebe50fbc541ce6f069ed5
AgusFerreira4 <ezeferreiraagustin@gmail.com>
2025-09-08 13:54:53 -0300

    Se borro el duplicado de la clase leer que estaba en main

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -14,24 +14,3 @@ namespace Ucu.Poo.GameOfLife
         }
     }
 }
-// Cumple SRP porque tiene la unica responsabilidad de leer y de parsear.
-// Cumple expert porque es la parte del codigo con la que se puede parsear 1/0 en una matriz bool[,] sin necesidad de otras clases
-public class LeerArchivo
-{
-    public  bool[,] Archivo(string url)
-    {
-        string content = File.ReadAllText(url);
-        string[] contentLines = content.Split('\n');
-        bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
-        for (int y=0; y<contentLines.Length;y++)
-        {
-            for (int x=0; x<contentLines[y].Length; x++)
-            {
-                if(contentLines[y][x]=='1')
-                {
-                    board[x,y]=true;
-                }
-            }
-        } return board;
-    }   
-}
