a1c70092709c67471e281b2324dcc2868f673a8c
Ignacio Rodriguez <ignaciorodriguezsantos5@gmail.com>
2025-09-08 10:11:43 -0300

    me-falto-hacer-push

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,11 @@ namespace Ucu.Poo.GameOfLife
     {
         static void Main(string[] args)
         {
-           
+            LeerArchivo archivo = new LeerArchivo();
+            bool[,] tableroInicial = archivo.Archivo("board.txt");
+            int width = tableroInicial.GetLength(0);
+            int height = tableroInicial.GetLength(1);
+            PrintBoard.Print(tableroInicial, width, height);
         }
     }
 }
